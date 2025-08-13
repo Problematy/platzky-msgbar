10fda40bd9bf9988f5ba0ab3ad3259116bc32740
Milena Chowaniec <cho.milena123@gmail.com>
2025-08-14 00:49:46 +0200

    cypress confi and tests update

--- tests/e2e_tests/cypress.config.js
+++ tests/e2e_tests/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5000/',
+    baseUrl: 'http://127.0.0.1:5000',
     setupNodeEvents(on, config) {
       return config
     },
