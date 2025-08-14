51d3679b61e53186e8d538cde294937030447fd4
Milena Chowaniec <cho.milena123@gmail.com>
2025-08-14 11:18:10 +0200

    tests changes

--- tests/e2e_tests/e2e_app.py
+++ tests/e2e_tests/e2e_app.py
@@ -1,11 +1,21 @@
 from platzky.platzky import create_app as base_create_app
+from platzky_msgbar.entrypoint import process as msgbar_process
+from flask import Flask, render_template_string
 
 
 def create_app(config_path: str):
     app = base_create_app(config_path=config_path)
 
-    @app.route("/")
+    msgbar_process(app, {"message": "Hello from MsgBar!"})
+
     def index():
-        return "Hello from Flask (E2E)"
+        # Make sure the MsgBar element exists in the page for Cypress
+        return render_template_string("""
+            <html>
+            <body>
+                <div id="MsgBar">{{ app.config['msgbar']['message'] }}</div>
+            </body>
+            </html>
+        """, app=app)
 
     return app
