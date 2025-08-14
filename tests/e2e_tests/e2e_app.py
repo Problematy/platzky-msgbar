ceace101db182bb306989af667dde23fa9733ebf
Milena Chowaniec <cho.milena123@gmail.com>
2025-08-14 11:26:10 +0200

    tests changes

--- tests/e2e_tests/e2e_app.py
+++ tests/e2e_tests/e2e_app.py
@@ -1,25 +1,30 @@
+from flask import render_template_string
 from platzky.platzky import create_app as base_create_app
 from platzky_msgbar.entrypoint import process as msgbar_process
-from flask import render_template_string
 
 
 def create_app(config_path: str):
+    # Base app
     app = base_create_app(config_path=config_path)
 
+    # Inject MsgBar plugin with a test message
     msgbar_process(app, {"message": "Hello from MsgBar!"})
 
     @app.route("/")
     def index():
-        # Make sure the MsgBar element exists in the page for Cypress
+        # Minimal HTML with <head> so MsgBar plugin can inject CSS + div
         return render_template_string(
             """
             <html>
+            <head>
+                <title>E2E Test Page</title>
+            </head>
             <body>
-                <div id="MsgBar">{{ app.config['msgbar']['message'] }}</div>
+                <h1>Welcome to E2E Test</h1>
+                <p>This page includes the MsgBar plugin for testing.</p>
             </body>
             </html>
-        """,
-            app=app,
+        """
         )
 
     return app
